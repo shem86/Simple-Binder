b96fda3e0df6ee165dfea359497f4ba92bc87208
Shem86 <Shem86@gmail.com>
2014-07-04 19:34:23 +0300

    rearranged files

---
+++ simple-binder/binder.js
@@ -0,0 +1,22 @@
+define('binder', ['jquery'], function ($) {
+	//Binder object
+	var Binder = function (id) {
+		this.eventAggregator = $({});
+		var self = this;
+		var dataAttribute = "bind-" + id,
+			message = id + ":change";
+
+		$(document).on('change', "[data-" + dataAttribute + "]", function (event) {
+			var $input = $(this);
+			self.eventAggregator.trigger(message, [$input.data(dataAttribute), $input.val()]);
+		});
+		this.eventAggregator.on(message, function (event, property, value) {
+			$("[data-" + dataAttribute + "=" + property + "]" ).each( function() {
+				$(this).val(value);
+			});
+		});
+		return this.eventAggregator;
+	}
+
+	return Binder;
+});
